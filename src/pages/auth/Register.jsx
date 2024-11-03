6e3340972207e758292296a891f5089d755d198d
Sanket Banerjee <sanketbanerjee.2004@gmail.com>
2024-11-03 17:09:01 +0530

    phone input width fixed

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -106,7 +106,7 @@ function Register() {
                                 Phone number
                             </label>
                             <div className="mt-2 flex">
-                                <div className="w-max rounded-s-md border-0 px-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:outline-none focus:ring-inset focus:ring-sky-600 sm:text-sm/6">
+                                <div className="w-max rounded-s-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:outline-none focus:ring-inset focus:ring-sky-600 sm:text-sm/6">
                                     +91
                                 </div>
                                 <input
@@ -118,7 +118,7 @@ function Register() {
                                     value={phone}
                                     onChange={(e) => setPhone(e.target.value)}
                                     autoComplete="phone"
-                                    className="block w-[85%] rounded-e-md border-0 px-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:outline-none focus:ring-inset focus:ring-sky-600 sm:text-sm/6"
+                                    className="block w-full rounded-e-md border-0 px-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:outline-none focus:ring-inset focus:ring-sky-600 sm:text-sm/6"
                                 />
                             </div>
                         </div>
